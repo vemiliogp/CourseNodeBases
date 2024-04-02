51e6f5e78bd71e263117000e7a74428edc3a93b7
Victor E González P <vemiliogp@gmail.com>
2024-04-02 13:40:18 +0200

    Second commit

--- src/app.js
+++ src/app.js
@@ -1,18 +1,12 @@
-// const { getUUID, getAge } = require("./plugins");
+// const getPokemonById = require("./js-foundation/06-promises");
+const { buildLogger } = require("./plugins");
 
-// const { emailTemplate } = require("./js-foundation/01-template");
-// require("./js-foundation/02-destructuring");
-// const { getUserById } = require("./js-foundation/03-callbacks");
-// const { getUserById } = require("./js-foundation/04-arrow");
-// const { buildMakePerson } = require("./js-foundation/05-factory");
-const getPokemonById = require("./js-foundation/06-promises");
+const logger = buildLogger("app.js");
 
-getPokemonById(4)
-  .then((pokemon) => console.log({ pokemon }))
-  .catch((error) => console.log(error))
-  .finally(() => console.log("Finally"));
+logger.log("Hola mundo");
+logger.error("Esto es algo malo");
 
-// const makePerson = buildMakePerson({ getAge, getUUID });
-// const obj = { name: "John", birthDate: "1995-10-26" };
-// const john = makePerson(obj);
-// console.log(john);
+// getPokemonById(4)
+//   .then((pokemon) => console.log({ pokemon }))
+//   .catch((error) => console.log(error))
+//   .finally(() => console.log("Finally"));
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,9 +1,11 @@
 const { getAge } = require("./get-age.plugin");
 const { getUUID } = require("./get-id.plugin");
 const { http } = require("./http-client.plugin");
+const buildLogger = require("../plugins/logger.plugin");
 
 module.exports = {
   getAge,
   getUUID,
+  buildLogger,
   http,
 };
